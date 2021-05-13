5e9c9316d6980182716b39802849547905aa0d72
Ignacio Pieve <ignacio.pieve@gmail.com>
2021-05-13 16:13:06 -0300

    All Done.

--- src/sample/MainPruebas.java
+++ src/sample/MainPruebas.java
@@ -1,14 +1,12 @@
 package sample;
 
-import distribuciones.Distribucion;
-import distribuciones.Exponencial;
-import distribuciones.Uniforme;
-
-import java.math.BigInteger;
-
 public class MainPruebas {
     public static void main(String[] args) throws Exception {
-        System.out.println(new BigInteger("21.5"));
-    }
+        double numero;
 
+        for (int i = 0 ; i < 2000000000 ; i++)
+        {
+            numero = Math.random();
+        }
+    }
 }
